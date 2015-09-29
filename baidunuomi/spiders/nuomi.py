2420b88717f41d1d6a6bb53a6fdf6871e540eb38
leearic <leearic@126.com>
2015-09-29 17:45:53 +0800

    能够爬去一些数据，但是还没放到数据库中运行. 去除了 proxy. 官方有防爬措施.使用proxy的方式不能爬到数据

--- baidunuomi/spiders/nuomi.py
+++ baidunuomi/spiders/nuomi.py
@@ -1,21 +1,70 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import scrapy,time
 from scrapy.selector import Selector
-
-
-
-
+from scrapy.http import Request
 class ExampleSpider(scrapy.Spider):
     name = "nuomi"
     allowed_domains = ["nuomi.com"]
     start_urls = (
         'http://www.nuomi.com/pcindex/main/changecity',
     )
-
     def parse(self, response):
         sel = Selector(response)
-        # 开始选择地区
-        zone_link = sel.xpath('//ul[@class="cities fl"]/li/a/@href').extract()
-        zone_name = sel.xpath('//ul[@class="cities fl"]/li/a/strong/text()').extract()
-        print zone_link
+        # 根据选择地区页面,获取到地区和地区的二级页面.并将地区和连接对应起来
+        zones = sel.xpath('//ul[@class="cities fl"]/li')
+        for index in zones:
+            zone_link = index.xpath('a/@href').extract()
+            zone_text = index.xpath('a/strong/text()').extract()
+            if isinstance(zone_text, list) and len(zone_text) != 0:
+               yield Request(url=zone_link[0], callback=self.parse_zone,  meta={'zone':zone_text[0]})
 
+    def parse_zone(self, response):
+        # 1. 将地区通过参数传进来
+        # 2. 到地区页面去解析分类
+        zone = response.meta['zone']
+        sel = Selector(response)
+        categorys = sel.xpath('//div[@class="level-item"]/div/dl')
+        for index in categorys:
+            category_link = index.xpath('dt[@class="title"]/a/@href').extract()
+            category_text = index.xpath('dt[@class="title"]/a/text()').extract()
+            if "nuomi.com" not in category_link[0]:
+                pass
+            else:
+                yield  Request(url=category_link[0], callback=self.parse_category,  meta={'zone':zone, 'category': category_text})
+
+    def parse_category(self, response):
+        zone = response.meta['zone']
+        category = response.meta['category']
+        sel = Selector(response)
+        # 获取详细页面连接地址
+        pages = sel.xpath('//ul[@class="itemlist clearfix"]/li/a/@href').extract()
+        for i in pages:
+            if "nuomi.com" not in i:
+                pass
+            else:
+                yield Request(url=i, callback=self.parse_page, meta={'zone':zone, 'category': category})
+        # 获取到分页
+        page_list = sel.xpath('//div[@class="pager-wrap"]/div[@class="ui-pager"]/a/@href').extract()
+        for i in page_list:
+            yield Request(url=i, callback=self.parse_category,  meta={'zone':zone, 'category': category})
+
+    def parse_page(self, response):
+        sel = Selector(response)
+        product_zone = response.meta['zone']
+        product_category =  response.meta['category']
+        product_name = sel.xpath('//div[@class="w-item-info clearfix"]/h2/text()').extract()
+        product_detail = sel.xpath('//div[@class="item-title"]/span/text()').extract()
+        product_star = sel.xpath('//div[@class="score fl"]/text()').extract()
+        product_price = sel.xpath('//div[@class="match-price-area"]/span[@class="real-price"]/text()').extract()
+        #### product_address = sel.xpath('//li[@class="branch branch-open"]/p[@class="branch-address"]/text()').extract() # js解析
+        product_images = sel.xpath('//p[@class="wrap-img"]/img/@src').extract()
+
+        print product_zone
+        print product_category[0].encode('gbk')
+        print product_name[0].encode('gbk')
+        print product_detail[0].encode('gbk')
+        print product_star
+        print product_price[0]
+        for i in product_images:
+            print i
+        print "================================="
