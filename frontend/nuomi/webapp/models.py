519656212721df362c352746a3db924ab253380a
leearic <leearic@126.com>
2015-09-30 14:37:11 +0800

    可以爬去部分数据,准备加入数据库支持

--- frontend/nuomi/webapp/models.py
+++ frontend/nuomi/webapp/models.py
@@ -1,26 +1,18 @@
 # -*- coding: utf-8
 from django.db import models
 # Create your models here.
-
-
 class Zone(models.Model):
     zone = models.CharField(max_length=255, verbose_name=u"地区")
 
 class Category(models.Model):
     category = models.CharField(max_length=255, verbose_name=u"分类")
-#    product_zone = models.ForeignKey(Zone, related_name='product_zone')
 
 class Product(models.Model):
      product_name = models.CharField(max_length=255, verbose_name=u"产品名称")
-    # aa
      product_category = models.ForeignKey(Category, related_name='product_category')
      product_zone = models.ForeignKey(Zone, related_name='product_zone')
 
-
 class ProductDetail(models.Model):
-#    product_name = models.CharField(max_length=255, verbose_name=u"产品名称")
-#    product_category = models.ForeignKey(Category, related_name='product_category')
-#    product_zone = models.ForeignKey(Zone, related_name='product_zone')
     product = models.ForeignKey(Product, related_name='product_detail_id')
     product_price = models.FloatField(max_length=8, verbose_name=u"产品价格")
     product_star = models.FloatField(max_length=3, verbose_name=u"产品好评度")
